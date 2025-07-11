8c57660ec4d4a36c9250de16e705df19451161ed
Bruno_local <brunocesar.br@gmail.com>
2025-07-11 13:08:29 -0300

    Django Models (Banco de Dados) e Admin Interface
    - Criado as tabelas do banco de dados
    - Observação: Para adição do campo ImageField na tabela é necessario a instalação do Pillow para gestão de imagens.
    - Registrado os models para que os campos do app members apareçam no painel de administrador: Em my_tennis_club/members/admin.py adicione from .models import Member e admin.site.register(Member)
    - Código para criação das tabelas: python manage.py makemigrations members e python manage.py migrate
    - Ativar o Servidor Django: python manage.py runserver

--- W3Schools/members/models.py
+++ W3Schools/members/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=255, verbose_name='Primeiro Nome')
+    lastname = models.CharField(max_length=255, verbose_name='Sobrenome')
+    email = models.EmailField(unique=True, verbose_name='E-mail')
+    phone = models.CharField(max_length=20, blank=True, null=True, verbose_name='Telefone')
+    birth_date = models.DateField(blank=True, null=True, verbose_name='Data de Nascimento')
+    gender = models.CharField(
+        max_length=1,
+        choices=[('M', 'Masculino'), ('F', 'Feminino'), ('O', 'Outro')],
+        blank=True,
+        null=True,
+        verbose_name='Gênero'
+    )
+    address = models.TextField(blank=True, null=True, verbose_name='Endereço')
+    # profile_picture = models.ImageField(upload_to='members/photos/', blank=True, null=True, verbose_name='Foto de Perfil')
+    is_active = models.BooleanField(default=True, verbose_name='Ativo')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Criado em')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Atualizado em')
+
+    class Meta:
+        ordering = ['firstname']
+        verbose_name = 'Membro'
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname}"
