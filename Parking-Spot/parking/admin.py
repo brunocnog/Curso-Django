6832b14804870b4a11e6e30287f314e6fe738f37
Bruno_local <brunocesar.br@gmail.com>
2025-07-16 14:08:23 -0300

    Retirado do painel a possibilidade de alocar um veículo a uma vaga já ocupada e de aparecer um veículo que já esta alocado a uma vaga
    - Em Parking-Spot\parking\admin.py criada a função def formfield_for_foreignkey(self, db_field, request, **kwargs):

--- Parking-Spot/parking/admin.py
+++ Parking-Spot/parking/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from parking.models import ParkingRecord, ParkingSpot
+from vehicles.models import Vehicle
 
 
 @admin.register(ParkingSpot)
@@ -8,7 +9,26 @@ class ParkingSpotAdmin(admin.ModelAdmin):
     search_fields = ['spot_number']
     list_filter = ['is_occupied']
 
+
 @admin.register(ParkingRecord)
 class ParkingRecordAdmin(admin.ModelAdmin):
     list_display = ['vehicle', 'parking_spot', 'entry_time', 'exit_time']
     search_fields = ['vehicle__license_plate', 'parking_spot__spot_number']
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+
+        is_editing = request.resolver_match.url_name.endswith('change')
+
+        if db_field.name == "parking_spot" and not is_editing:
+            # Só mostra vagas que não estão ocupadas
+            kwargs["queryset"] = ParkingSpot.objects.filter(is_occupied=False)
+
+        elif db_field.name == 'vehicle' and not is_editing:
+            # Pega IDs dos veículos que já estão estacionados (sem horário de saída)
+            vehicle_is_parked = ParkingRecord.objects.filter(
+                exit_time__isnull=True).values_list('vehicle_id', flat=True)
+            # Mostra apenas veículos que não estão atualmente estacionados
+            kwargs["queryset"] = Vehicle.objects.exclude(
+                id__in=vehicle_is_parked)
+
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
