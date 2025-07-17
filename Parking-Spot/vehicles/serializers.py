64454c2b11fb61ada201118a64230110a1f35aa6
Bruno_local <brunocesar.br@gmail.com>
2025-07-17 15:49:54 -0300

    Ajustes finais na APT

--- Parking-Spot/vehicles/serializers.py
+++ Parking-Spot/vehicles/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 
-from vehicles.models import Vehicle, VehicleType
+from vehicles.models import VehicleType, Vehicle
+
+
+class VehicleTypeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = VehicleType
+        fields = '__all__'
 
 
 class VehicleSerializer(serializers.ModelSerializer):
@@ -9,9 +16,3 @@ class VehicleSerializer(serializers.ModelSerializer):
         model = Vehicle
         fields = '__all__'
 
-
-class VehicleTypeSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = VehicleType
-        fields = '__all__'
--- Parking-Spot/vehicles/views.py
+++ Parking-Spot/vehicles/views.py
@@ -1,15 +1,14 @@
 from rest_framework import viewsets
 
-from vehicles.models import Vehicle, VehicleType
-from vehicles.serializers import VehicleSerializer, VehicleTypeSerializer
-
-
-class VehicleViewSet(viewsets.ModelViewSet):
-    queryset = Vehicle.objects.all()
-    serializer_class = VehicleSerializer
+from vehicles.models import VehicleType, Vehicle
+from vehicles.serializers import VehicleTypeSerializer, VehicleSerializer
 
 
 class VehicleTypeViewSet(viewsets.ModelViewSet):
     queryset = VehicleType.objects.all()
     serializer_class = VehicleTypeSerializer
-    
+
+
+class VehicleViewSet(viewsets.ModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
