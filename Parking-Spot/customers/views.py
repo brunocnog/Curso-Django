4223e2d3ba2be2df3bc100a27bd7eedc85cdbba4
Bruno_local <brunocesar.br@gmail.com>
2025-07-18 14:39:58 -0300

    Configurado Permissões para Admin
    - Adicionado DjangoModelPermissions e IsAdminUser  em customers\views.py
    - Dado permissões de leitura para o usuário Felipe pelo Django Admin

--- Parking-Spot/customers/views.py
+++ Parking-Spot/customers/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import DjangoModelPermissions, IsAdminUser
 
 from customers.models import Customer
 from customers.serializers import CustomerSerializer
@@ -7,3 +8,4 @@ from customers.serializers import CustomerSerializer
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [DjangoModelPermissions, IsAdminUser]
