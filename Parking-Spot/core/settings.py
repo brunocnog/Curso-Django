68904f1d28ac3e0e8b4dc13d25b8e8c5ec12df82
Bruno_local <brunocesar.br@gmail.com>
2025-07-21 13:37:05 -0300

    Instalado o psycopg2-binary
    - Biblioteca de conexão python com o bando de dados postgres
    - pip install psycopg2-binary
    - Atualizado o requirements: pip freeze > requirements.txt
    - Atualizado o DATABASES em core\settings.py para uso do banco de dados postgres

--- Parking-Spot/core/settings.py
+++ Parking-Spot/core/settings.py
@@ -85,11 +85,14 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'parking-spot',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'parkiparking_db',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
