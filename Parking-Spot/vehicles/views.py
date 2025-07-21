6f81070c34b63c9fa4660984d4bdfcdfa48e04c2
Bruno_local <brunocesar.br@gmail.com>
2025-07-21 09:10:48 -0300

    Configurando Permissões
    - Agora cada usuário só poderia ver as informações respectivas ao seu veículo
    - Criado o arquivo core\permissions.py em core.
    - Criado a classe IsOwnerOfVehicleOrRecord()

--- Parking-Spot/vehicles/views.py
+++ Parking-Spot/vehicles/views.py
@@ -1,5 +1,7 @@
+from rest_framework.permissions import DjangoModelPermissions, IsAdminUser
 from rest_framework import viewsets
 
+from core.permissions import IsOwnerOfVehicleOrRecord
 from vehicles.models import VehicleType, Vehicle
 from vehicles.serializers import VehicleTypeSerializer, VehicleSerializer
 
@@ -7,8 +9,16 @@ from vehicles.serializers import VehicleTypeSerializer, VehicleSerializer
 class VehicleTypeViewSet(viewsets.ModelViewSet):
     queryset = VehicleType.objects.all()
     serializer_class = VehicleTypeSerializer
+    permission_classes = [DjangoModelPermissions, IsAdminUser]
 
 
 class VehicleViewSet(viewsets.ModelViewSet):
     queryset = Vehicle.objects.all()
     serializer_class = VehicleSerializer
+    permission_classes = [DjangoModelPermissions, IsOwnerOfVehicleOrRecord]
+    
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Vehicle.objects.all()
+        return Vehicle.objects.filter(owner__user = user)
